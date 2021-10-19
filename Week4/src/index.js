3b9259ffb1f2917bfed7125ab7c33fff367f55bc
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-10-19 15:27:33 +0200

    Week4 -nodes

---
+++ Week4/src/index.js
@@ -0,0 +1,11 @@
+
+
+
+var cleave = new Cleave('.input-phone', {
+    phone: true,
+    phoneRegionCode: '{country}'
+});
+
+var Cleave = require('cleave.js');
+require('cleave.js/dist/addons/cleave-phone.{country}');
+ 
