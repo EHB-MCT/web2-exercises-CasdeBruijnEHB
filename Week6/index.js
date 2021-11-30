22d851d671365e5bbfed866bbe2ac32fc7227d50
CasdeBruijnEHB <71756854+CasdeBruijnEHB@users.noreply.github.com>
2021-11-30 10:42:18 +0100

    post update

--- Week6/index.js
+++ Week6/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+/*
+====================================
+==============EXC 1=================
+====================================
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
@@ -19,6 +22,44 @@ app.get('/getdata', (req, res) => {
         jerseynumber: 24
     }
     res.send(exampledata)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+*/
+/*
+====================================
+==============EXC 2=================
+====================================
+*/
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    console.log("local root called");
+    res.redirect('/info.html')
+})
+app.get('/api/boardgames', (req, res) => {
+    fetch('./boardgames.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log("aye");
+            res.send(data);  
+        })
+})
+app.get('/api/boardgame', (req, res) => {
+    fetch('./api/boardgame?ID=120677')
+        .then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
+})
+app.post('/api/saveboardgame', (req, res) => {
+    fetch('/api/boardgame?ID=120677')
+        .then(response => response.json())
+        .then(data => {
+            res.send(data);
+        })
 })
 
 app.listen(port, () => {
